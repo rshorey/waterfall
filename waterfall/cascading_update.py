e65d8600ac16ce4a6b0f8c2d396df525d87b8c81
Rachel Shorey <rshorey@gmail.com>
2015-05-28 13:52:47 -0400

    no longer postgres specific, but a bit less tidy

--- waterfall/cascading_update.py
+++ waterfall/cascading_update.py
@@ -1,46 +1,29 @@
-from django.db import connection
+from django.db import connection, models
 from django.apps.registry import apps
 from django.db.utils import IntegrityError
 
 class CascadingUpdate():
-    
-    def __init__(self):
-        self.table_modelnames = self.get_table_modelnames()
-
-    def get_table_modelnames(self):
-        table_modelnames = {}
-        for m in apps.get_models(include_auto_created=True):
-            table_modelnames[m._meta.db_table] = m
-        return table_modelnames
 
     def get_fk_list(self, obj):
-        cursor = connection.cursor()
-        update_cursor = connection.cursor()
-        table_name = obj._meta.db_table
-        sql = """select s_class.relname source_table,
-                att.attname source_name
-                from pg_constraint cons join pg_class f_class
-                on cons.confrelid = f_class.oid
-                join pg_class s_class
-                on cons.conrelid = s_class.oid
-                join pg_attribute att
-                on att.attnum = ANY (cons.conkey)
-                and att.attrelid = s_class.oid
-                where f_class.relname = %s
-                and cons.contype = 'f'"""
+        for m in apps.get_models(include_auto_created=True):
+            for f in m._meta.get_all_field_names():
+                try:
+                    relation = m._meta.get_field(f).rel
+                except models.fields.FieldDoesNotExist:
+                    continue
+                if relation:
+                    if relation.to == obj.__class__:
+                        yield(m, f)
 
-        cursor.execute(sql, [table_name])
-        return cursor
 
-    def get_related_objects(self, obj, related_tables):
+
+    def get_related_objects(self, obj, related_models):
         #returns an iterator of tuples (object, keyname)
         app_name = obj._meta.app_label
-        for table, keyname in related_tables:
-            model_type = self.table_modelnames[table]
-            related_objects = model_type.objects.filter(**{keyname:obj.id})
+        for model_type, keyname in related_models:
+            related_objects = model_type.objects.filter(**{keyname:obj})
             for r in related_objects:
                 yield (r, keyname)
-
 
 
 
@@ -54,14 +37,12 @@ class CascadingUpdate():
             for k in list(related_obj_dict.keys()):
                 if k.startswith("_") or k == 'id':
                     del related_obj_dict[k]
-            related_obj_dict[keyname] = new_obj.id
+            related_obj_dict[keyname+"_id"] = new_obj.id
             if related_class.objects.filter(**related_obj_dict).count() > 0:
                 related_obj.delete()
             else:
-                setattr(related_obj, keyname, new_obj.id)
+                setattr(related_obj, keyname, new_obj)
                 related_obj.save()
-
-
 
     def merge_foreign_keys(self, obj_to_remove, persistent_obj):
         fk_relations = self.get_fk_list(obj_to_remove)
